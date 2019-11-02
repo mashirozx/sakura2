b02591c02f89dbfac49ea09bef8d0e3a6cecbf1e
Mashiro <adadam@qq.com>
2019-11-02 10:58:21 +0800

    remove ts-loader

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -30,6 +30,6 @@ import Parallax from 'parallax-js'
 //let scene = document.getElementById('cover-img-container')
 //let parallaxInstance = new Parallax(scene)
 
-// let ele: HTMLImageElement = document.getElementById("cover-img") as HTMLImageElement
-// let eleW: number = ele.naturalWidth
-// console.log(eleW)
+let ele: HTMLImageElement = document.querySelector("#cover-img") as HTMLImageElement
+let eleW: number = ele.naturalWidth
+console.log(eleW)
--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,6 @@ module.exports = [{
       {
         test: /\.tsx?$/,
         use: [
-          { loader: 'ts-loader' },
           { 
             loader: 'babel-loader',
             query: {
