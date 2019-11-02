6dc4eccd564910d9bd3109c738266acc59a5482b
Mashiro <adadam@qq.com>
2019-11-03 03:03:07 +0800

    auto commit 11/03/2019 Sun

--- src/ts/components/MDCHandler.ts
+++ src/ts/components/MDCHandler.ts
@@ -3,20 +3,20 @@
  * @author Mashiro
  * @since 19/11/1
  */
-import { MDCRipple } from '@material/ripple/index'
+import { MDCRipple } from '@material/ripple'
 import { MDCTextField } from '@material/textfield'
 import { MDCTopAppBar } from '@material/top-app-bar'
 
 export default function () {
-    interface LooseObject {
-        [key: string]: any
-    }
+    // const textField = new MDCTextField(document.querySelector('.mdc-text-field'))
+    
+    //const ripple = new MDCRipple(document.querySelector('.mdc-button'))
+    MDCRipple.attachTo(document.querySelector('.demo-card__primary-action'))
+    MDCRipple.attachTo(document.querySelector('.mdc-button'))
+    
+    // const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'))
 
-    let MDC: LooseObject = {}
-
-    const topAppBarElement = document.querySelector('.mdc-top-app-bar')
-    const topAppBar = new MDCTopAppBar(topAppBarElement)
-
-    // const textField = new MDCRipple(document.querySelector('.mdc-button'))
-    // if(textField) const textField = new MDCTextField(document.querySelector('.mdc-text-field'))
+    const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function (el) {
+        return new MDCTextField(el)
+    })
 }
