6fe483289d89a831fdf93c170a515edeb5107140
Mashiro <adadam@qq.com>
2019-11-04 03:13:55 +0800

    auto commit 11/04/2019 Mon

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -8,14 +8,35 @@
 import coverImgIni from "./modules/coverImgInit"
 import headerBarScrollHandler from "./modules/headerBarScrollHandler"
 import mdcInit from "./components/mdcInit"
+import Pjax from "pjax"
 
 
 window.onscroll = function () {
   headerBarScrollHandler()
 }
 
-window.onload = function () {
+let InitFun = function () {
   coverImgIni()
   mdcInit()
+}
 
+window.onload = function () {
+  InitFun()
+
+  let pjax = new Pjax({
+    elements: "a", // default is "a[href], form[action]"
+    selectors: ["title", ".root"],
+    cacheBust: true
+  })
+
+  function pjaxReload() {
+    console.log('pjax:complete')
+    if (window.location.pathname == "/") {
+      console.log(`home!`)
+    } else {
+      console.log(`not home!`)
+    }
+    InitFun()
+  }
+  document.addEventListener('pjax:complete', pjaxReload)
 }
--- src/ts/modules/coverImgInit.ts
+++ src/ts/modules/coverImgInit.ts
@@ -12,81 +12,86 @@ import Parallax from 'parallax-js'
  * Include Parallax box initial and `#cover-img-container` size initial.
  */
 export default function () {
-    let coverImgContainer = <HTMLElement>document.querySelector("#cover-img-container")
-    coverImgContainer.style.height = `${window.innerHeight}px`
     let headerTopAfter = <HTMLElement>document.querySelector("#header-top-after")
-    headerTopAfter.style.height = `${window.innerHeight}px`
-
-    interface LooseObject {
-        [key: string]: any
+    if (typeof (headerTopAfter) !== 'undefined' && headerTopAfter !== null) {
+        headerTopAfter.style.height = `${window.innerHeight}px`
     }
+    
+    let coverImgContainer = <HTMLElement>document.querySelector("#cover-img-container")
+    if (typeof (coverImgContainer) !== 'undefined' && coverImgContainer !== null) {
+        coverImgContainer.style.height = `${window.innerHeight}px`
 
-    /**
-     * cover: `img#cover-img`
-     */
-    let coverImg: LooseObject = {}
-    /**
-     * mark: `figure#cover-img-container`
-     */
-    let coverBox: LooseObject = {}
-    /**
-     * layer: `div#img-view`
-     */
-    let coverView: LooseObject = {}
+        interface LooseObject {
+            [key: string]: any
+        }
 
-    coverImg.e = <HTMLImageElement>document.querySelector("#cover-img")
-    coverImg.w = <number>coverImg.e.naturalWidth
-    coverImg.h = <number>coverImg.e.naturalHeight
+        /**
+         * cover: `img#cover-img`
+         */
+        let coverImg: LooseObject = {}
+        /**
+         * mark: `figure#cover-img-container`
+         */
+        let coverBox: LooseObject = {}
+        /**
+         * layer: `div#img-view`
+         */
+        let coverView: LooseObject = {}
 
-    coverBox.e = <HTMLImageElement>document.querySelector("#cover-img-container")
-    coverBox.w = <number>coverBox.e.offsetWidth
-    coverBox.h = <number>coverBox.e.offsetHeight
+        coverImg.e = <HTMLImageElement>document.querySelector("#cover-img")
+        coverImg.w = <number>coverImg.e.naturalWidth
+        coverImg.h = <number>coverImg.e.naturalHeight
 
-    coverView.e = <HTMLImageElement>document.querySelector("#img-view")
+        coverBox.e = <HTMLImageElement>document.querySelector("#cover-img-container")
+        coverBox.w = <number>coverBox.e.offsetWidth
+        coverBox.h = <number>coverBox.e.offsetHeight
 
-    coverBox.e.style.height = `${window.innerHeight}px`
+        coverView.e = <HTMLImageElement>document.querySelector("#img-view")
 
-    coverBox.f = (coverBox.w >= 1000 || coverBox.h >= 1000) ? 1000 : 500
-    if (coverBox.w >= coverBox.h) {
-        coverBox.i = coverBox.w / coverBox.f * 50;
-        coverBox.y = coverBox.i;
-        coverBox.x = coverBox.i * coverBox.w / coverBox.h;
-    } else {
-        coverBox.i = coverBox.h / coverBox.f * 50;
-        coverBox.x = coverBox.i;
-        coverBox.y = coverBox.i * coverBox.h / coverBox.w;
-    }
+        coverBox.e.style.height = `${window.innerHeight}px`
 
-    coverView.e.style.cssText = `
+        coverBox.f = (coverBox.w >= 1000 || coverBox.h >= 1000) ? 1000 : 500
+        if (coverBox.w >= coverBox.h) {
+            coverBox.i = coverBox.w / coverBox.f * 50;
+            coverBox.y = coverBox.i;
+            coverBox.x = coverBox.i * coverBox.w / coverBox.h;
+        } else {
+            coverBox.i = coverBox.h / coverBox.f * 50;
+            coverBox.x = coverBox.i;
+            coverBox.y = coverBox.i * coverBox.h / coverBox.w;
+        }
+
+        coverView.e.style.cssText = `
         width: ${coverBox.w + coverBox.x}px; 
         height: ${coverBox.h + coverBox.y}px; 
         margin-left: ${-0.5 * coverBox.x}px; 
         margin-top: ${-0.5 * coverBox.y}px`
 
-    coverImg.e.style.cssText = `
+        coverImg.e.style.cssText = `
         width: ${coverView.w + coverView.x}px;
         height: ${coverView.h + coverView.y}px`
 
-    if (!coverImg.w) {
-        coverImg.w = coverImg.e.offsetWidth
-        coverImg.h = coverImg.e.offsetHeight;
+        if (!coverImg.w) {
+            coverImg.w = coverImg.e.offsetWidth
+            coverImg.h = coverImg.e.offsetHeight;
+        }
+
+        coverImg._w = coverImg.e.parentElement.offsetWidth
+        coverImg._h = coverImg.e.parentElement.offsetHeight
+        coverImg.ratio = coverImg.h / coverImg.w
+
+        if (coverImg._h / coverImg._w > coverImg.ratio) {
+            coverImg.e.style.height = coverImg._h + 'px';
+            coverImg.e.style.width = coverImg._h / coverImg.ratio + 'px';
+        } else {
+            coverImg.e.style.width = coverImg._w + 'px';
+            coverImg.e.style.height = coverImg._w * coverImg.ratio + 'px';
+        }
+
+        coverImg.e.style.left = (coverImg._w - parseInt(coverImg.e.style.width)) / 2 + 'px';
+        coverImg.e.style.top = (coverImg._h - parseInt(coverImg.e.style.height)) / 2 + 'px';
+
+        let scene = document.querySelector('#cover-img-container')
+        let parallaxInstance = new Parallax(scene);
     }
-
-    coverImg._w = coverImg.e.parentElement.offsetWidth
-    coverImg._h = coverImg.e.parentElement.offsetHeight
-    coverImg.ratio = coverImg.h / coverImg.w
-
-    if (coverImg._h / coverImg._w > coverImg.ratio) {
-        coverImg.e.style.height = coverImg._h + 'px';
-        coverImg.e.style.width = coverImg._h / coverImg.ratio + 'px';
-    } else {
-        coverImg.e.style.width = coverImg._w + 'px';
-        coverImg.e.style.height = coverImg._w * coverImg.ratio + 'px';
-    }
-
-    coverImg.e.style.left = (coverImg._w - parseInt(coverImg.e.style.width)) / 2 + 'px';
-    coverImg.e.style.top = (coverImg._h - parseInt(coverImg.e.style.height)) / 2 + 'px';
-
-    let scene = document.querySelector('#cover-img-container')
-    let parallaxInstance = new Parallax(scene);
 }
